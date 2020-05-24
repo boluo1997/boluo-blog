55ec16bf57f0784caa1c5eb6053e6fbb3cd8d783
dingchao <1109928059@qq.com>
2020-05-24 16:59:20 +0800

    Initial commit

---
+++ src/main/java/com/boluo/boluoblog/BoluoBlogApplication.java
@@ -0,0 +1,13 @@
+package com.boluo.boluoblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoluoBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoluoBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boluo/boluoblog/BoluoBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boluo.boluoblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoluoBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
