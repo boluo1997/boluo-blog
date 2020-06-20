292c5c2615a4c0cf5f1079d0fd04838b2e4cf6e4
dingchao <1109928059@qq.com>
2020-06-20 23:55:17 +0800

    明天建表

--- src/main/java/com/boluo/boluoblog/domain/Article.java
+++ src/main/java/com/boluo/boluoblog/domain/Article.java
@@ -4,13 +4,13 @@ import java.util.Date;
 
 public class Article {
 
-    private Integer id;              //文章主键ID
+    private Integer id;             //文章主键ID
+
+    private Integer uid;            //发布人ID
 
     private String title;           //标题
 
     private String image;           //标题图片
-
-    //内容缩略名
 
     private Date cTime;             //文章创建时间
 
@@ -18,31 +18,130 @@ public class Article {
 
     private String content;         //文章文字内容
 
-    //内容所属用户ID
-
     private String type;            //文章分类
 
-    private String tag;                 //标签
+    private String tag;             //标签
 
-    //内容状态
+    private Integer hits;           //点击次数
 
-    //分类列表
+    private Integer likes;          //点赞数
 
-    private Integer hits;               //点击次数
+    private Integer comments;       //评论数
 
-    private Integer likes;              //点赞数
+    private Integer status;         //状态码
 
-    //内容所属评论数
-
-    //是否允许评论
-
-    //是否允许ping
-
-    //允许出现在聚合中
-
-    private Integer status;             //状态码
-
-    private Integer isdelete;           //是否删除
+    private Integer isdelete;       //是否删除
 
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Date getcTime() {
+        return cTime;
+    }
+
+    public void setcTime(Date cTime) {
+        this.cTime = cTime;
+    }
+
+    public Date getmTime() {
+        return mTime;
+    }
+
+    public void setmTime(Date mTime) {
+        this.mTime = mTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Integer getHits() {
+        return hits;
+    }
+
+    public void setHits(Integer hits) {
+        this.hits = hits;
+    }
+
+    public Integer getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+
+    public Integer getComments() {
+        return comments;
+    }
+
+    public void setComments(Integer comments) {
+        this.comments = comments;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getIsdelete() {
+        return isdelete;
+    }
+
+    public void setIsdelete(Integer isdelete) {
+        this.isdelete = isdelete;
+    }
 }
