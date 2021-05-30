ca6aaa68f1692236eb6be3e8242bcd385934499f
boluo1997 <1109928059@qq.com>
2021-05-30 19:11:12 +0800

    基本返回类型

---
+++ src/main/java/com/boluo/blog/common/BaseResponse.java
@@ -0,0 +1,45 @@
+package com.boluo.blog.common;
+
+// import io.swagger.annotations.ApiModel;
+import com.boluo.blog.enums.SystemResultCode;
+
+import java.io.Serializable;
+
+// @ApiModel(description = "响应参数")
+public class BaseResponse<T> implements Serializable {
+
+    private static final long serialVersionUID = -6259241733322453783L;
+
+    private ResultInfo resultInfo;
+
+    private T data;
+
+    public BaseResponse() {
+        resultInfo = new ResultInfo();
+    }
+
+    public BaseResponse(T data) {
+        this();
+        this.data = data;
+    }
+
+    public ResultInfo getResultInfo() {
+        return resultInfo;
+    }
+
+    public void setResultInfo(ResultInfo resultInfo) {
+        this.resultInfo = resultInfo;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public void setResultEnum(SystemResultCode resultEnum) {
+        this.resultInfo.setResultEnum(resultEnum);
+    }
+}
